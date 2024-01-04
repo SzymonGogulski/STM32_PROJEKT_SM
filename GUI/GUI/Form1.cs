e6c7d79a6cf4ea2d9e508148413b32587b961325
Alan Grądecki <alangradecki@wp.pl>
2024-01-04 19:10:35 +0100

    poprawiono błąd związany z wątkami

--- GUI/GUI/Form1.cs
+++ GUI/GUI/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -75,9 +75,14 @@ namespace GUI
                 // wyświetlanie aktualnej temperatury
                 lblCurrentTemperature.Text = dataIn;
 
-                if (double.TryParse(dataIn, out temperature))
+                if (double.TryParse(dataIn.Replace('.', ','), out temperature))
                 {
                     UpdateChart(temperature);
+                }
+                else
+                {
+                    MessageBox.Show("Błąd aktualizowania wykresu!", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
                 }
             });
         }
